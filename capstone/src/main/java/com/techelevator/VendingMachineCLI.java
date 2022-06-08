213215d681f69f98e150ab0a52998cc656b3bab6
Dave Hoy <david.hoy@techelevator.com>
2022-06-08 09:21:44 -0400

    Initial commit

---
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class VendingMachineCLI {
+
+	public VendingMachineCLI() {
+	}
+
+	public static void main(String[] args) {
+		VendingMachineCLI cli = new VendingMachineCLI();
+		cli.run();
+	}
+
+	public void run() {
+	}
+
+}
